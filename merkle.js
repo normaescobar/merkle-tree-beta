6b1fbcbbf0af48a4a13052372c1479757ad38e9f
Norma Escobar <64007837+normaescobar@users.noreply.github.com>
2022-12-21 00:41:52 -0330

    Add files via upload

---
+++ merkle.js
@@ -0,0 +1,16 @@
+class MerkleTree{
+    constructor(data){
+      this.data = data;
+      this.nodes = [];
+      this.root = null;
+    }
+  
+    create(){
+      let data = this.data;
+      for (let i = 0; i < data.length; i++) {
+        let node = new Node(data[i]);
+        this.nodes.push(node);
+      }
+      while (this.nodes.length > 1) {
+         let newNodes = [];
+         for (let i = 0; i < this.nodes.length; i += 2)
